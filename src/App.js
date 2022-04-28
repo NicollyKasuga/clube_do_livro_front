51b679c5f2b13e40e9f9db8af53f47ae1f180ff4
Nicky <nicollyporfirio12@gmail.com>
2022-04-28 12:16:55 -0300

    feature: NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import GlobalStyle from './GlobalStyle/style';
+import {Routes} from './Routes/index'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Routes/>
     </div>
   );
 }
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,13 +1,14 @@
 import { Switch, Route } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Dashboard } from "../Pages/Dashboard/indes";
 
-function Routes() {
-    const history = useHistory();
+export function Routes() {
+     const history = useHistory();
 
     return(
         <Switch>
             <Route exact path="/">
-
+                <Dashboard/>
             </Route>
             <Route path="/Entrar">
 
