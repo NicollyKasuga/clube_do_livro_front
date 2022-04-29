6b0280ce673f854cf178bad57a375cebbf4a00e8
Nicky <nicollyporfirio12@gmail.com>
2022-04-29 05:52:48 -0300

    added: chat feature path

---
+++ src/Components/Chat/style.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const ChatClosed = styled.button`
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #DEDEDE;
+    position: absolute;
+    bottom: 0px;
+    right: 30px;
+    border-radius: 4px 4px 0px 0px;
+
+    .chat_icon{
+        color: #878888;
+        height: 20px;
+        width: 30px;
+    }
+`
+
+export const ChatOpened = styled.div`
+    width: 380px;
+    height: 300px;
+    background: #DEDEDE;
+    position: absolute;
+    bottom: 0px;
+    right: 50px;
+    border-radius: 6px 6px 0px 0px;
+`
+
+export const UsersContainer = styled.div`
+
+`
+
+export const NavSearch = styled.div`
+
+`
+
+export const ListOfUsers = styled.div`
+
+`
+
+export const ChatDisplay = styled.div`
+
+`
+
+export const HeaderChat = styled.div`
+
+`
+
+export const MessagesContainer = styled.div`
+
+`
+
+export const InputContainer = styled.div`
+
+`
