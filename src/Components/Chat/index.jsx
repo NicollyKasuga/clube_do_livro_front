572d5a70ba61f19b705ee8652778db80b43f84fa
Nicky <nicollyporfirio12@gmail.com>
2022-04-29 10:07:05 -0300

    update: added getAttribute on handleClick

--- src/Components/Chat/index.jsx
+++ src/Components/Chat/index.jsx
@@ -1,10 +1,13 @@
 import {ChatClosed, ChatOpened, UsersContainer, NavSearch, ListOfUsers, ChatDisplay, HeaderChat, MessagesContainer, InputContainer} from './style'
 import {BsFillChatLeftDotsFill} from 'react-icons/bs'
+import { CardUser } from '../CardUser'
 import { useState } from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 
 export const Chat = () =>{
 
+    const users = [{'nome': "Robertinho Gameplays", 'id':1}, {'nome': "Robertinho Gameplays", 'id':2}, {'nome': "Robertinho Gameplays", 'id':2}]
     const [chatOpened, setChatOpened] = useState(false)
 
     function handleClick(){
@@ -19,8 +22,18 @@ export const Chat = () =>{
         (
             <ChatOpened>
                 <UsersContainer>
-                    <NavSearch></NavSearch>
-                    <ListOfUsers></ListOfUsers>
+                    <NavSearch>
+                        <div className='div_input'>
+                        <AiOutlineSearch className='search_icon'/>
+                        <input type="text" placeholder="Nome da pesquisa..."></input>
+                        </div>
+                    </NavSearch>
+                    <ListOfUsers>
+                        {users.map((user, index) =>(
+                            
+                            <CardUser user={user.nome} key={index} id={user.id}/>
+                        ))}
+                    </ListOfUsers>
                 </UsersContainer>
                 <ChatDisplay>
                     <HeaderChat></HeaderChat>
--- src/Components/Chat/style.js
+++ src/Components/Chat/style.js
@@ -20,24 +20,67 @@ export const ChatClosed = styled.button`
 `
 
 export const ChatOpened = styled.div`
-    width: 380px;
+    width: 450px;
     height: 300px;
     background: #DEDEDE;
     position: absolute;
     bottom: 0px;
     right: 50px;
     border-radius: 6px 6px 0px 0px;
+    display: flex;
 `
 
 export const UsersContainer = styled.div`
-
+    width: 45%;
+    height: 100%;
+    bacground: #DEDEDE;
+    border-radius: 6px 6px 0px 0px;
+    display: flex;
+    flex-direction: column;
 `
 
 export const NavSearch = styled.div`
+    width: 100%;
+    height: 25%;
+    border-radius: 6px 6px 0px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
+    .div_input{
+        width: 80%;
+        height: 50%;
+        background: #95D6D6;
+        display: flex;
+        align-items: center;
+        border-radius: 5px;
+
+        .search_icon{
+            position: absolute;
+            left: 26px;
+            color: white;
+        }
+
+        input{
+            width: 100%;
+            height: 80%;
+            padding-left: 25px;
+            box-sizing: border-box;
+            background: #95D6D6;
+            color: white;
+            font-weight: 700;
+        }
+    }
+
+   
+
+   
 `
 
 export const ListOfUsers = styled.div`
+    width: 100%;
+    height: 75%;
+    background: green;
 
 `
 
