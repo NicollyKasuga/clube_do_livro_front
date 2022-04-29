7466df14cd51a8e12b39532ccab2c242dcc23257
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-04-29 16:16:50 -0300

    update: change endpoint on requests according api

--- src/Components/FormLogin/index.jsx
+++ src/Components/FormLogin/index.jsx
@@ -1,51 +1,64 @@
-import { Form, LineDiv } from "./style"
-import {Input} from '../Input/index'
-import {Button} from '../Button/index'
-import * as yup from "yup"
-import { useForm } from "react-hook-form"
-import {yupResolver} from "@hookform/resolvers/yup"
-import api from '../../Service/index'
-import { toast } from "react-toastify"
+import { Form, LineDiv } from './style';
+import { Input } from '../Input/index';
+import { Button } from '../Button/index';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import api from '../../Service/index';
+import { toast } from 'react-toastify';
 
-export const FormLogin = ({history, setAuthenticated}) => {
+export const FormLogin = ({ history, setAuthenticated }) => {
+  const formSchema = yup.object().shape({
+    email: yup.string().required('Email obrigatório').email('Email inválido'),
+    password: yup.string().required('Insira uma senha'),
+  });
 
-    const formSchema = yup.object().shape({
-        email: yup.string().required("Email obrigatório").email("Email inválido"),
-        password: yup.string().required("Insira uma senha")
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
 
-    const{
-        register,
-        handleSubmit,
-        formState: {errors},
-    } = useForm({
-        resolver: yupResolver(formSchema)
-    })
+  function handleLogin(data) {
+    console.log(data);
 
-    function handleLogin(data) {
-        console.log(data);
-        
-        api.post("/sessions", data)
-        .then(response => {
-            const {token} = response.data;
+    api
+      .post('/entrar', data)
+      .then((response) => {
+        const { access_token } = response.data;
 
-            localStorage.setItem("@Clube_do_livro:", JSON.stringify(token));
-            setAuthenticated(true)
+        localStorage.setItem('@Clube_do_livro:token', access_token);
+        setAuthenticated(true);
 
-            return history.push("/");
-        })
-        .catch((err) => toast.error("Email ou senha inválidos"));
-    };
+        return history.push('/');
+      })
+      .catch((err) => toast.error('Email ou senha inválidos'));
+  }
 
-
-    return (
-        <>
-        <Form onSubmit={handleSubmit(handleLogin)}>
-            <Input type="text" placeholder="Digite seu email" label="Email:" register={register} data={"email"} error={errors.email?.message}/>
-            <Input type="password" placeholder="Digite sua senha" label="Senha:" register={register} data={"password"} error={errors.password?.message}/>
-            <LineDiv/>
-            <Button text="Entrar"/>
-        </Form>
-        </>
-    )
-}
+  return (
+    <>
+      <Form onSubmit={handleSubmit(handleLogin)}>
+        <Input
+          type="text"
+          placeholder="Digite seu email"
+          label="Email:"
+          register={register}
+          data={'email'}
+          error={errors.email?.message}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua senha"
+          label="Senha:"
+          register={register}
+          data={'password'}
+          error={errors.password?.message}
+        />
+        <LineDiv />
+        <Button text="Entrar" />
+      </Form>
+    </>
+  );
+};
--- src/Components/FormSignup/index.jsx
+++ src/Components/FormSignup/index.jsx
@@ -1,41 +1,85 @@
-import {Form} from './style'
-import {Input} from '../../Components/Input/index'
-import {Button} from '../../Components/Button/index'
-import * as yup from 'yup'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { Form } from './style';
+import { Input } from '../../Components/Input/index';
+import { Button } from '../../Components/Button/index';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import api from '../../Service/index';
+import { toast } from 'react-toastify';
 
+export const FormSignup = ({ history }) => {
+  const formSchema = yup.object().shape({
+    name: yup.string().required('Nome obrigatório*'),
+    email: yup.string().required('Email obrigatório*').email('Email inválido*'),
+    password: yup
+      .string()
+      .required('Senha obrigatória*')
+      .min(6, 'Senha fraca*'),
+    confirm_password: yup
+      .string()
+      .required('Confrime sua senha*')
+      .oneOf([yup.ref('password'), null], 'Senhas diferentes*'),
+  });
 
-export const FormSignup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
 
-    const formSchema = yup.object().shape({
-        name: yup.string().required("Nome obrigatório*"),
-        email: yup.string().required("Email obrigatório*").email("Email inválido*"),
-        password: yup.string().required("Senha obrigatória*").min(6, "Senha fraca*"),
-        confirm_password: yup.string().required("Confrime sua senha*").oneOf([yup.ref("password"), null], "Senhas diferentes*")
-    });
+  function handleSignup(data) {
+    // const { name, email, password } = data;
+    delete data.confirm_password;
+    console.log(data);
+    api
+      .post('/cadastro', data)
+      .then((response) => {
+        toast.success('Usuário criado');
 
-    const{
-        register,
-        handleSubmit,
-        formState: {errors},
-    } = useForm({
-        resolver: yupResolver(formSchema)
-    });
+        history.push('/entrar');
+      })
+      .catch((err) => toast.error('Erro ao criar usuário'));
+  }
 
-    function handleSignup(data){
-        const {name, email, password, confirm_password} = data;
-    };
-
-    return(
-        <>
-        <Form onSubmit={handleSubmit(handleSignup)}>
-            <Input label="Nome:" placeholder="Digite seu nome" type="text" register={register} data="name" error={errors.name?.message}/>
-            <Input label="Email:" placeholder="Digite seu email" type="text" register={register} data="email" error={errors.email?.message}/>
-            <Input label="Senha:" placeholder="Digite sua senha" type="password" register={register} data="password" error={errors.password?.message}/>
-            <Input label="Confirme sua senha:" placeholder="Confirme sua senha" type="password" register={register} data="password" error={errors.confirm_password?.message}/>
-            <Button text='Cadastrar'/>
-        </Form>
-        </>
-    )
-}
+  return (
+    <>
+      <Form onSubmit={handleSubmit(handleSignup)}>
+        <Input
+          label="Nome:"
+          placeholder="Digite seu nome"
+          type="text"
+          register={register}
+          data="name"
+          error={errors.name?.message}
+        />
+        <Input
+          label="Email:"
+          placeholder="Digite seu email"
+          type="text"
+          register={register}
+          data="email"
+          error={errors.email?.message}
+        />
+        <Input
+          label="Senha:"
+          placeholder="Digite sua senha"
+          type="password"
+          register={register}
+          data="password"
+          error={errors.password?.message}
+        />
+        <Input
+          label="Confirme sua senha:"
+          placeholder="Confirme sua senha"
+          type="password"
+          register={register}
+          data="confirm_password"
+          error={errors.confirm_password?.message}
+        />
+        <Button text="Cadastrar" />
+      </Form>
+    </>
+  );
+};
