fa6af38cd07e9296c923d33c1d808cf77386136a
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-05-01 16:15:26 -0300

    update: update identation

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,22 +1,23 @@
-import {Container, FormDiv} from './style'
-import {ContainerImg} from '../../Components/ContainerImg/index'
-import {FormLogin} from '../../Components/FormLogin/index'
-import { Link, Redirect } from 'react-router-dom'
+import { Container, FormDiv } from './style';
+import { ContainerImg } from '../../Components/ContainerImg/index';
+import { FormLogin } from '../../Components/FormLogin/index';
+import { Link, Redirect } from 'react-router-dom';
 
-export const Login = ({history, authenticated, setAuthenticated}) => {
+export const Login = ({ history, authenticated, setAuthenticated }) => {
+  if (authenticated) {
+    return <Redirect to="/" />;
+  }
 
-    if (authenticated){
-        return <Redirect to="/"/>
-    }
-
-    return(
-        <Container>
-        <ContainerImg/>
-        <FormDiv>
-            <h1>Bem-vindo</h1>
-            <FormLogin history={history} setAuthenticated={setAuthenticated}/>
-            <p>Não tem uma conta? <Link to="/Cadastro">Cadastre-se!</Link></p>
-        </FormDiv>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ContainerImg />
+      <FormDiv>
+        <h1>Bem-vindo</h1>
+        <FormLogin history={history} setAuthenticated={setAuthenticated} />
+        <p>
+          Não tem uma conta? <Link to="/Cadastro">Cadastre-se!</Link>
+        </p>
+      </FormDiv>
+    </Container>
+  );
+};
